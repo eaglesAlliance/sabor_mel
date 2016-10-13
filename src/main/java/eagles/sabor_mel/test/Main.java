0d99262eeef45225bebc2a20d1f78a4a2fd78598
etivideo <etivideo@conferencia>
2016-10-13 16:55:47 -0300

    Create a new persist test

--- src/main/java/eagles/sabor_mel/test/Main.java
+++ src/main/java/eagles/sabor_mel/test/Main.java
@@ -8,39 +8,80 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
+        /*Insert into Estado
         EstadoDAO dao = new EstadoDAO();
-        CidadeDAO cid = new CidadeDAO();
-        BairroDAO bai = new BairroDAO();
-        /*Insert Estado and Cidade*/
+        
+        Estado estado = new Estado("São Paulo", "SP");
+        
+        dao.persist(estado);
+        */
+        
+        Calendar c = Calendar.getInstance();
+        c.set(1988, 3, 21);
+        
+        EstadoDAO      estDAO = new EstadoDAO();
+        CidadeDAO      cidDAO = new CidadeDAO();
+        BairroDAO      baiDAO = new BairroDAO();
+        EnderecoDAO    endDAO = new EnderecoDAO();
+        PessoaDAO      pesDAO = new PessoaDAO();
+        DocumentoDAO   docDAO = new DocumentoDAO();
+        FuncionarioDAO funDAO = new FuncionarioDAO();
+        TelefoneDAO    telDAO = new TelefoneDAO();
+        
+        Estado estado           = new Estado("São Paulo", "SP");
+        Cidade cidade           = new Cidade("Caraguatatuba");
+        Bairro bairro           = new Bairro("Pontal de Santa Marina");
+        Endereco endereco       = new Endereco("Rua do Contorno", "199", "11672-020");
+        Pessoa pessoa           = new Pessoa("Tiago Lima", "tiago@gmail.com", c);
+        Documento documento     = new Documento("340.124.578-37", "CPF");
+        Funcionario funcionario = new Funcionario("login", "admin123", "A");
+        Telefone telefone       = new Telefone("(12)", "3887-9006", "F");
+        
+        
+        cidade.setEstado(estado);
+        bairro.setCidade(cidade);
+        endereco.setBairro(bairro);
+        pessoa.setEndereco(endereco);
+        pessoa.setDocumento(documento);
+        pessoa.addTelefone(telefone);
+        funcionario.setPessoa(pessoa);
+        
+        funDAO.persist(funcionario);
+       
+        
+        
+        
+       /* Bairro bairro = new Bairro("Pontal", cidade);
+        cidade.addBairro(bairro);
+        
+        Endereco endereco = new Endereco("RUA", "10", "00", bairro);
+        bairro.addEndereco(endereco);
+        
+        Documento documento = new Documento("100", "CPF");
+        
+        Pessoa pessoa = new Pessoa("Tiago", "tiago@gmail.com", c, documento, endereco);
+        documento.setPessoa(pessoa);
+        endereco.setPessoa(pessoa);
+        
+        Funcionario funcionario = new Funcionario("login", "senha", "V", pessoa);
+        
+        
+        Telefone telefone = new Telefone("12", "38879006", "fixo");
+        telefone.setPessoa(pessoa);
+        pessoa.addTelefone(telefone);*/
         
        
-        /*Estado estado = new Estado("São Paulo", "SP");
-       
-       
-        Cidade cidade = new Cidade("Caraguatatuba", estado);
-        estado.addCidade(cidade);
-       
-        dao.persist(estado);
-        cid.persist(cidade);*/
-       
-       
-       
-        List<Cidade> cidades = cid.findAll();
-       
-        for(int i = 0; i < cidades.size(); i++){
-            Bairro b1 = new Bairro("Barranco Alto", cidades.get(i));
+        /*baiDAO.persist(bairro);
+        endDAO.persist(endereco);
+        docDAO.persist(documento);
+        telDAO.persist(telefone);
+        pesDAO.persist(pessoa);
+        funDAO.persist(funcionario);*/
+            
             
 
-            cidades.get(i).addBairro(b1);
-            cid.persist(cidades.get(i));
-            bai.persist(b1);
-            System.out.println(cidades.get(i).getCidade());
-        }
-            
-      
-       
-       
-       
-       System.exit(0);
+        
+
+        System.exit(0);
     }
 }
