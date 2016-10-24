661453d52af87f6fcc774e7fdb913bd10fea9a57
etivideo <etivideo@conferencia>
2016-10-24 16:50:58 -0200

    Create new main for tests

---
+++ src/main/java/eagles/sabor_mel/test/Temp.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eagles.sabor_mel.test;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+/**
+ *
+ * @author etivideo
+ */
+public class Temp {
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+        String senha = "admin";
+                     
+        MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
+        byte messageDigest[] = algorithm.digest(senha.getBytes("UTF-8"));
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : messageDigest) {
+          hexString.append(String.format("%02X", 0xFF & b));
+        }
+        String senhahex = hexString.toString();
+
+        System.out.println(senhahex);
+    }
+}
