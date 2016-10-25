d603ee4e768f559409d1a734a623862efeabffbc
etivideo <etivideo@conferencia>
2016-10-25 16:30:53 -0200

    Clear the Temp Class

--- src/main/java/eagles/sabor_mel/test/Temp.java
+++ src/main/java/eagles/sabor_mel/test/Temp.java
@@ -5,28 +5,16 @@
  */
 package eagles.sabor_mel.test;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import eagles.sabor_mel.dao.TelefoneDAO;
+
 
 /**
  *
- * @author etivideo
+ * @author Tiago Lima Villalobos
+ * 
  */
 public class Temp {
-    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException{
-        String senha = "admin";
-                     
-        MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
-        byte messageDigest[] = algorithm.digest(senha.getBytes("UTF-8"));
-
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : messageDigest) {
-          hexString.append(String.format("%02X", 0xFF & b));
-        }
-        String senhahex = hexString.toString();
-
-        System.out.println(senhahex);
+    public static void main(String[] args){
+        
     }
 }
