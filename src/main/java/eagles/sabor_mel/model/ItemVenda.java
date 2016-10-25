4c1f5a2711b04cdff8d20f35efca281ec435b33f
a1655086 <a1655086@LAB117M27.lab117.campus.ifsp>
2016-10-25 20:30:50 -0200

    Vendas; Fornecedor; Fornecimento; Crediario; Parcela; Control Vendas; Control Pessoa;

---
+++ src/main/java/eagles/sabor_mel/model/ItemVenda.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eagles.sabor_mel.model;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author dhiego.balthazar
+ */
+
+@Entity
+@Table(name = "item_venda")
+public class ItemVenda implements Serializable{
+    
+    @Id
+    @SequenceGenerator(name = "idItemVenda", sequenceName = "idItemVenda_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idItemVenda")
+    @Column(name = "idItemVenda", nullable = false)
+    private Long idItemVenda;
+    
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
+    
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idProduto", nullable = false)
+    private Produto produto;
+    
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idVenda", nullable = false)
+    private Venda venda;
+
+    public Long getIdItemVenda() {
+        return idItemVenda;
+    }
+
+    public void setIdItemVenda(Long idItemVenda) {
+        this.idItemVenda = idItemVenda;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+    
+    
+}
---
+++ src/main/java/eagles/sabor_mel/model/Parcela.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eagles.sabor_mel.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author dhiego.balthazar
+ */
+
+@Entity
+@Table(name = "parcela")
+public class Parcela implements Serializable{
+    
+    @Id
+    @SequenceGenerator(name = "idParcela", sequenceName = "idParcela_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idParcela")
+    @Column(name = "idParcela", nullable = false)
+    private Long idParcela;
+    
+    @Column(name = "valorParcela", nullable = false)
+    private Double valorParcela;
+    
+    @Column(name = "parcela", nullable = false)
+    private Integer parcela;
+    
+    @Column(name = "statusParcela", nullable = false)
+    private String status;
+    
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idCrediario", nullable = false)
+    private Crediario crediario;
+    
+    @Column(name = "dataVencimento", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar dataVencimento;
+
+    public Long getIdParcela() {
+        return idParcela;
+    }
+
+    public void setIdParcela(Long idParcela) {
+        this.idParcela = idParcela;
+    }
+
+    public Double getValorParcela() {
+        return valorParcela;
+    }
+
+    public void setValorParcela(Double valorParcela) {
+        this.valorParcela = valorParcela;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getParcela() {
+        return parcela;
+    }
+
+    public void setParcela(Integer parcela) {
+        this.parcela = parcela;
+    }
+    
+    public Crediario getCrediario() {
+        return crediario;
+    }
+
+    public void setCrediario(Crediario crediario) {
+        this.crediario = crediario;
+    }
+
+    public Calendar getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Calendar dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+    
+    public boolean estaPago(){
+        return getStatus().equals("Sim");
+    }
+}
