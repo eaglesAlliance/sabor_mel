9f026438a65dc77b0a907108edaab3004c300004
etivideo <etivideo@conferencia>
2016-11-01 16:40:14 -0200

    Create new tests

--- src/main/java/eagles/sabor_mel/test/Temp.java
+++ src/main/java/eagles/sabor_mel/test/Temp.java
@@ -5,7 +5,17 @@
  */
 package eagles.sabor_mel.test;
 
-import eagles.sabor_mel.dao.TelefoneDAO;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import javax.swing.JFileChooser;
 
 
 /**
@@ -14,7 +24,47 @@ import eagles.sabor_mel.dao.TelefoneDAO;
  * 
  */
 public class Temp {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException, IOException, URISyntaxException{
+//        JFileChooser arquivo = new JFileChooser();
+//
+//        int retorno = arquivo.showOpenDialog(arquivo);
+//        String caminhoArquivo = "";
+//        String nome = "";
+//
+//        if (retorno == JFileChooser.APPROVE_OPTION) {
+//            caminhoArquivo = arquivo.getSelectedFile().getAbsolutePath();
+//            nome = arquivo.getSelectedFile().getName();
+//            System.out.println(caminhoArquivo);
+//            System.out.println(nome);
+//            File selecionado = arquivo.getSelectedFile();
+//            
+//            /**/
+//            FileInputStream origem;
+//            FileOutputStream destino;
+//            FileChannel fcOrigem;
+//            FileChannel fcDestino;
+//            
+//            origem = new FileInputStream(caminhoArquivo);
+//            destino = new FileOutputStream("C:\\Users\\etivideo\\Documents\\"+nome);
+//            
+//            fcOrigem = origem.getChannel();
+//            fcDestino = destino.getChannel();
+//            
+//            fcOrigem.transferTo(0, fcOrigem.size(), fcDestino);
+//            
+//            origem.close();
+//            destino.close();
+            /**/
+//        }
+
+//        Double b = 25d;
+//        String a = "R$ 25,50";
+//        
+//        a = a.replace("R$", "").replace(",", ".");
+//        s
+//        System.out.println(a);
+//        System.out.println(Double.parseDouble(a)+b);
+        
         
     }
 }
